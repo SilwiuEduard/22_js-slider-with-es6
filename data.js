570b5e37d5882ca773e931e521dd2f4e450c8def
SilwiuEduard <silviu.chiriloaie.it@gmail.com>
2023-05-25 23:16:22 +0200

    small changes

--- data.js
+++ data.js
@@ -1,5 +1,11 @@
 const people = [
   {
+    img: "https://scontent-ams4-1.xx.fbcdn.net/v/t39.30808-6/277364517_5323080687702169_9143269673041769026_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=wKH4zeiNA_IAX9GqNjb&_nc_ht=scontent-ams4-1.xx&oh=00_AfDBYOWAXPTu6ffDYG10vwGTF7Mkq4RRWa3zpJ8QjbWrWw&oe=6474F61C",
+    name: "silviu eduard",
+    job: "developer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
     img: "https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg",
     name: "peter doe",
     job: "product manager",
@@ -15,12 +21,6 @@ const people = [
     img: "https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg",
     name: "emma doe",
     job: "designer",
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
-  },
-  {
-    img: "https://scontent-ams4-1.xx.fbcdn.net/v/t39.30808-6/277364517_5323080687702169_9143269673041769026_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=wKH4zeiNA_IAX9GqNjb&_nc_ht=scontent-ams4-1.xx&oh=00_AfDBYOWAXPTu6ffDYG10vwGTF7Mkq4RRWa3zpJ8QjbWrWw&oe=6474F61C",
-    name: "silviu eduard",
-    job: "developer",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
 ];
